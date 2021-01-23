2b50232a6e8c640d5af00fb218f3da6afe850eaa
riccardo <sicchieririccardo@gmail.com>
2021-01-23 10:51:49 +0100

    start login task

--- databse/index.php
+++ databse/index.php
@@ -1,7 +1,5 @@
 <?php 
-include('include/config.php');
-include('createDatabase.php');
-include('createTables.php');
+
  ?>
 <html>
     <head>
@@ -9,8 +7,6 @@ include('createTables.php');
         <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
     <body>
-        <?php include('templates/header.php'); ?>
-        <?php include('templates/menu.php'); ?>
 
         <h1>Libri</h1>
         <p>Tanti bei libri</p>
