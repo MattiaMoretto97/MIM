6a53f1f0ac85c22f6dc6a67d5a361531e422fe68
riccardo <sicchieririccardo@gmail.com>
2021-01-23 12:10:54 +0100

    First pull on branch

---
+++ database/index.php
@@ -0,0 +1,42 @@
+<?php 
+
+ ?>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Bookique</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+    </head>
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+      <div class="container">
+        <form class="">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="form-group">
+                <label for="user">Username:</label>
+                <input type="text" class="form-control">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="form-group w-100">
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control"placeholder="">
+              </div>
+            </div>
+        </form>
+      </div>
+   
+ 
+
+
+        
+    </body>
+</html>
