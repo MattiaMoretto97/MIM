78d05b4e898d07fc85f9bcce05a9ff8c4ae8f48d
riccardo <sicchieririccardo@gmail.com>
2021-01-23 12:59:11 +0100

    prima pagina registrazione

--- database/index.php
+++ database/index.php
@@ -9,34 +9,24 @@
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 
     </head>
-    <div class="container">
-        <div class="row">
-            
-        </div>
-    </div>
-      <div class="container">
-        <form class="">
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="form-group">
-                <label for="user">Username:</label>
-                <input type="text" class="form-control">
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12">
-              <div class="form-group w-100">
-                <label for="pwd">Password:</label>
-                <input type="password" class="form-control"placeholder="">
-              </div>
-            </div>
-        </form>
-      </div>
-   
- 
-
-
-        
+    <div>
+            <h1>Registrati</h1>
+            <h2> ciao, inserisci le tue credenziali  </h2>
+            <form class="form-container form-group">
+                <label for="fname">username:</label>
+                <input type="text" id="fname" name="fname" placeholder="first name"><br>
+                <label for="lname">email:</label>
+                <input type="text" id="lname" name="lname" placeholder="last name"><br>
+                <label for="fname">password:</label>
+                <input type="text" id="fname" name="fname" placeholder="email"><br>
+                <label for="lname">conferma password:</label>
+                <input type="text" id="lname" name="lname" placeholder="phone"><br>
+                <section title="checkbox">
+                <div class="ckeckbox">
+                <input type="checkbox" value="None" id="squaredFour" name="check" checked />
+                <label for="squaredFour">Accetto i <a href="#">termini di condizione d'uso</a></label>
+                </div>
+                </section>
+              </form>
     </body>
 </html>
