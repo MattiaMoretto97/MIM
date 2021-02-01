7b2184ea02186557edf4d6bae3a143e410d8a1b9
federicocracco95 <fede.cracco95@gmail.com>
2021-02-01 19:44:58 +0100

    Work on home view

--- bookique/src/views/home.php
+++ bookique/src/views/home.php
@@ -8,10 +8,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Utente</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/navbarCss.css">
-    <link rel="stylesheet" href="css/searchBar.css">
+    <link rel="stylesheet" href="asset/css/custom/navbarCss.css">
+    <link rel="stylesheet" href="asset/css/custom/searchBar.css">
 </head>
 <body>
     <!-- Navbar -->
@@ -20,7 +20,7 @@
         <nav class="navbar">
             <div class="d-flex justify-content-start" id="NavBarLogo">
             <a class="navbar-brand" href="#">
-                <img src="img/logo.png" width="150" height="50" class="d-inline-block align-top" alt="">
+                <img src="asset/images/logo.png" width="150" height="50" class="d-inline-block align-top" alt="">
             </a>
             </div>
         </nav>
@@ -38,7 +38,7 @@
             </ul>
         </div>
         <div class="d-flex justify-content-end">
-            <a href="#">
+            <a href="#" data-toggle="modal" data-target="#exampleModal">
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle icon" viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
@@ -69,7 +69,7 @@
         <h2 style="margin: 50px;">Nuove Aggiunte</h2>
     </div>
     <div class="card border-dark" style="width: 18rem; margin:50px">
-        <img class="card-img-top" src="img/libro.jpg" alt="Card image cap">
+        <img class="card-img-top" src="asset/images/libro.jpg" alt="Card image cap">
         <div class="card-body" style="margin: auto;">
         <h5 class="card-title" style="text-align: center;">Titolo</h5>
         <button type="button" class="btn" href="#">Vedi dettaglio</button>
@@ -81,7 +81,7 @@
         <h2 style="margin: 50px;">Più visti</h2>
     </div>
     <div class="card border-dark" style="width: 18rem; margin:50px">
-        <img class="card-img-top" src="img/libro.jpg" alt="Card image cap">
+        <img class="card-img-top" src="asset/images/libro.jpg" alt="Card image cap">
         <div class="card-body" style="margin: auto;">
         <h5 class="card-title" style="text-align: center;">Titolo</h5>
         <button type="button" class="btn" href="#">Vedi dettaglio</button>
@@ -93,7 +93,7 @@
         <h2 style="margin: 50px;">Più letti</h2>
     </div>
     <div class="card border-dark" style="width: 18rem; margin:50px">
-        <img class="card-img-top" src="img/libro.jpg" alt="Card image cap">
+        <img class="card-img-top" src="asset/images/libro.jpg" alt="Card image cap">
         <div class="card-body" style="margin: auto;">
         <h5 class="card-title" style="text-align: center;">Titolo</h5>
         <button type="button" class="btn" href="#">Vedi dettaglio</button>
@@ -105,7 +105,7 @@
         <h2 style="margin: 50px;">Lista libri</h2>
     </div>
     <div class="card border-dark" style="width: 18rem; margin:50px">
-        <img class="card-img-top" src="img/libro.jpg" alt="Card image cap">
+        <img class="card-img-top" src="asset/images/libro.jpg" alt="Card image cap">
         <div class="card-body" style="margin: auto;">
         <h5 class="card-title" style="text-align: center;">Titolo</h5>
         <button type="button" class="btn" href="#">Vedi dettaglio</button>
@@ -119,23 +119,8 @@
             <a class="text-white" >Bookique</a>
         </div>
     </footer>
-    
-</body>
-</html>
-<html>
-    <head><title></title>
-        <link rel="stylesheet" href="maincambiopassword.css">
-        <link rel="stylesheet" type="text/css" href="asset/css/bootstrap/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="asset/css/custom/mainlogin.css">
-    </head>
-    <body>
-        <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-        clicca qui
-    </button>
-  
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -161,7 +146,9 @@
       </div>
     </div>
   </div>
-        <script  type="text/javascript" src="asset/js/jquery.js"></script>
-        <script type="text/javascript" src="asset/js/bootstrap.js"></script>
-    </body>
+
+  <script  type="text/javascript" src="asset/js/jquery.js"></script>
+  <script type="text/javascript" src="asset/js/bootstrap.js"></script>
+    
+</body>
 </html>
