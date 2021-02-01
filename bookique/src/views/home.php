6f38422585d70df3f4fd15c26c9dd498dca45428
riccardo <sicchieririccardo@gmail.com>
2021-02-01 20:37:53 +0100

    put a window modal in home.php

--- bookique/src/views/home.php
+++ bookique/src/views/home.php
@@ -119,28 +119,29 @@
             <a class="text-white" >Bookique</a>
         </div>
     </footer>
-
+    <!--modal window-->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">conferma password</h5>
+          <div class="modal-title" style="text-align:center"> 
+              <h3 class="titolo">cambia la password qui</h3>
+         </div>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-            <form>
-            <input type="text" id="lname" name="lname" placeholder="password"><br>
+            <form class="inserimento">
+            <input type="text" id="pname" name="lname" placeholder="password"><br>
             <label for="fname"></label>
             <input type="text" id="fname" name="fname" placeholder="nuova password"><br>
             <label for="lname"></label>
-            <input type="text" id="fname" name="fname" placeholder="conferma password"><br>
+            <input type="text" id="lname" name="fname" placeholder="conferma password"><br>
             <label for="lname"></label>
             </form>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
           <button type="button" class="btn btn-primary">INVIA</button>
         </div>
       </div>
