78b6c5bd3c66657c4140a925abb96128615af258
MattiaMoretto97 <mattiamoretto05@gmail.com>
2021-02-20 12:43:46 +0100

    fix problem

--- phpNormale/php/login.php
+++ phpNormale/php/login.php
@@ -1,13 +1,11 @@
 <?php
-<<<<<<< HEAD
 include "autoload.php";
 
 $cipher = env('cipher');
-print_r($cipher);
 $passphrase = env('passphrase');
 $options = env('options');
 $iv = env('iv');
-=======
+
 $servername = "localhost";
 $dsn = 'mysql:dbname=bookique;host=127.0.0.1';
 $username = "root";
@@ -21,77 +19,30 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$cipher="aes-256-cbc-hmac-sha256";
-$passphrase = 'ciao';
-$options = 0;
-$ivlen = openssl_cipher_iv_length($cipher);
-$iv = openssl_random_pseudo_bytes($ivlen);
->>>>>>> main
-
-$email = $_REQUEST['email'];
-$password = $_REQUEST['password'];
-
-$db = new PDO($dsn, $username, $password_a);
-$query_select = "SELECT password FROM utenti WHERE email = '".$email."'";
-
-$sth = $db->query($query_select);
-while( $row = $sth->fetch(PDO::FETCH_ASSOC) ) {
-  $password_db = $row; 
-}
-
-//print_r($password_db['password']);
-$prova = $password_db['password'];
-
-//print_r($prova);
-//print_r($cipher);
-//print_r($passphrase);
-//print_r($options);
-//print_r($iv);
-
-$password_db_dcr = openssl_decrypt($prova, $cipher, $passphrase, $options, $iv);
-
-<<<<<<< HEAD
 $email = $_REQUEST['email'];
 $password = openssl_encrypt($_REQUEST['password'], $cipher,$passphrase,$options,$iv);
-=======
-print_r($password_db_dcr);
->>>>>>> main
 
+$queryCheckUser = "SELECT * FROM utenti WHERE email = ? AND password = ?";
 
-if ($password_db_dcr === $password) {
-  
-  $queryCheckUser = "SELECT * FROM utenti WHERE email = ? AND password = ?";
-}
 
 $statement = mysqli_prepare($conn, $queryCheckUser);
 
-  $statement = mysqli_prepare($conn, $queryCheckUser);
+$statement = mysqli_prepare($conn, $queryCheckUser);
 
-  mysqli_stmt_bind_param($statement, "ss", $email, $password);
+mysqli_stmt_bind_param($statement, "ss", $email, $password);
 
-  mysqli_stmt_execute($statement);
+mysqli_stmt_execute($statement);
 
-  $result = mysqli_stmt_get_result($statement);
+$result = mysqli_stmt_get_result($statement);
 
-  $error = mysqli_stmt_error($statement);
+$error = mysqli_stmt_error($statement);
 
-  if ($error != null) {
-    echo $error;
-    die();
-  }
-
-print_r($count);
-
-if ($count == 0) {
-  die("Utente non trovato");
-  header("Location: login.php");
+if ($error != null) {
+  echo $error;
+  die();
 }
 
 $user = mysqli_fetch_assoc($result);
-<<<<<<< HEAD
-print_r($user);
-=======
->>>>>>> main
 if ($user['idRuolo'] == 2) {
 
   session_start();
@@ -109,5 +60,3 @@ if ($user['idRuolo'] == 2) {
   $_SESSION['superadmin'] = $user;
   header("Location: ../superadmin/homeSuperadmin.php");
 }
-
-?>
--- security/login.php
+++ security/login.php
@@ -4,13 +4,13 @@ $cipher =   env('cipher');
 $passphrase = env('passphrase');
 $options = env('options');
 $iv = openssl_random_pseudo_bytes(16);
-print_r($iv);
+print_r($iv) + "/n";
 $pippoticripto = openssl_encrypt('pippo', $cipher,$passphrase,$options,$iv);
-print_r($pippoticripto);
+print_r($pippoticripto); + "/n";
 
 $ivd = openssl_random_pseudo_bytes(16);
-print_r($ivd);
-$pippotidecripto = openssl_decrypt ( $pippoticripto , $cipher,$passphrase , $options, $ivd);
+print_r($ivd) + "/n";
+$pippotidecripto = openssl_decrypt ( $pippoticripto , $cipher,$passphrase , $options, $iv);
 
 print_r($pippotidecripto);
 ?>
