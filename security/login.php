c8e9d21be2f944909574f44c30a472ff6e3f6132
MattiaMoretto97 <mattiamoretto05@gmail.com>
2021-02-20 09:36:34 +0100

    decrypt function

--- security/login.php
+++ security/login.php
@@ -1,6 +1,13 @@
 <?php
-$ivlen = openssl_cipher_iv_length($cipher="aes-256-cbc-hmac-sha256");
+$cipher="aes-256-cbc-hmac-sha256";
+$passphrase = 'ciao';
+$options = 0;
+$ivlen = openssl_cipher_iv_length($cipher);
 $iv = openssl_random_pseudo_bytes($ivlen);
-$pippoticripto = openssl_encrypt('pippo', 'aes-256-cbc-hmac-sha256','ciao',0,$iv);
+$pippoticripto = openssl_encrypt('pippo', $cipher,$passphrase,$options,$iv);
 print_r($pippoticripto);
+
+$pippotidecripto = openssl_decrypt ( $pippoticripto , $cipher,$passphrase , $options, $iv);
+'<br>';
+print_r($pippotidecripto);
 ?>
